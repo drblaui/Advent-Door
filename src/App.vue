729c609470c2462e2ee09b258bef62ae809a5454
Alexander Rudolph <alexr30@web.de>
2022-11-25 02:06:36 +0100

    Attributions Link to repo fixed background

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <v-app style="background-color: #181a1b;">
+  <v-app style="height: 100vh;
+  background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
+  overflow: hidden;
+  filter: drop-shadow(0 0 10px white);">
     <v-main>
       <router-view />
     </v-main>
     <v-footer class="text-center bg-transparent">
       <v-row justify="center">
-        Source Code: <a href="" />
+        <a href="https://github.com/drblaui/Advent-Door" target="_blank"> Source Code</a>
       </v-row>
     </v-footer>
   </v-app>
